21ee518fb7e9ae95ddea2ff09bd1b9e2aaee1acc
AnzuRen <anzuren.id@gmail.com>
2025-04-19 21:30:57 +0700

    Add start_mobile_kas_server.bat 1745073055908

---
+++ start_mobile_kas_server.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Laragon compatible batch file to start the Apache server for Mobile Kas App
+
+echo Please open Laragon and start the Apache server manually.
+echo Ensure the project folder is placed inside Laragon's www directory.
+
+pause
