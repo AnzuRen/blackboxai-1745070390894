d8d69724649404132c091c63a31b48369f6cf9d5
AnzuRen <anzuren.id@gmail.com>
2025-04-19 20:46:54 +0700

    Add mobile-kas-app/resources/views/welcome.blade.php 1745070414107

---
+++ mobile-kas-app/resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-3xl font-bold mb-4">Welcome to Mobile Kas App</h1>
+    <p class="text-gray-400">This is a mobile-optimized dark mode Laravel app.</p>
+</div>
+@endsection
